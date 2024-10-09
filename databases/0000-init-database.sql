6d461e7f7eaeba4b1a2dd8ed8817ca23a5437cd8
Giã Dương Đức Minh <giaduongducminh@gmail.com>
2024-10-09 21:58:02 +0700

    database: added init database

---
+++ databases/0000-init-database.sql
@@ -0,0 +1,22 @@
+create table if not exists "event" (
+    "id" uuid primary key,
+    "name" text not null,
+    "no_tickets" int8 not null default 0,
+    "status" int2 not null default 0,
+    "created_at" timestamp not null default now(),
+    "created_by" text not null,
+    "updated_at" timestamp,
+    "updated_by" text not null
+);
+
+create table if not exists "event_txn" (
+    "id" uuid primary key,
+    "event_id" uuid not null,
+    "pg_txn_id" text, -- id from pg
+    "no_tickets" int8 not null default 0,
+    "status" int2 not null default 0,
+    "created_at" timestamp not null default now(),
+    "created_by" text not null,
+    "updated_at" timestamp,
+    "updated_by" text not null
+);
